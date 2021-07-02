34571b7bf0a57add68d9b6ba37823a2d52b96263
Msamson09 <michaelssamson@gmail.com>
2021-07-02 11:03:07 -0700

    includes some pseudocode in files

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
-console.log("sanity check")
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+// need variables for the 10 pokimon cards
+
+
+/*------------------------ Cached Element References ------------------------*/
+// need cached elements for the cards, player name, reset button
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+// need to create clicks for the cards, reset button, and player name
+
+
+/*-------------------------------- Functions --------------------------------*/
+// initialize the board
+    // reset board with all of the cards
+    // flip all of the cards over
+    // create a new random locations for the cards
+    // reset number of player turns
+
+// winning scenarios
+    // function to establish winning combinations 
+        // needs to be based on the shapes since the order will be random
+
+// render 
+
+
+// 
